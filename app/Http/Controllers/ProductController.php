a8c6e042210b517b8ebf9639930c5aafd428702e
David Scesnulevic <2016195@csl-student.wlv.ac.uk>
2023-04-07 12:31:28 +0100

    Order page created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
 use Session;
 use Illuminate\Support\Facades\DB;
 
@@ -55,6 +56,17 @@ class ProductController extends Controller
     function removeCart($id){
         Cart::destroy($id);
         return redirect('cartlist');
+    }
+
+    function orderNow()
+    {
+        $userId=Session::get('user')['id'];
+        $total= $products= DB::table('cart')
+         ->join('products','cart.product_id','=','products.id')
+         ->where('cart.user_id',$userId)
+         ->sum('products.price');
+ 
+         return view('ordernow',['total'=>$total]);
     }
 
     
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,4 @@ Route::get("/detail/{id}",[ProductController::class,'detail']);
 Route::get("/add_to_cart",[ProductController::class,'addToCart']);
 Route::get("cartlist",[ProductController::class,'cartlist']);
 Route::get("removecart/{id}",[ProductController::class,'removeCart']);
+Route::get("ordernow",[ProductController::class,'orderNow']);
