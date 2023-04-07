b3e0807047a39ea04aa0f006f04457cce0badb9a
David Scesnulevic <2016195@csl-student.wlv.ac.uk>
2023-04-07 09:52:20 +0100

    Product details page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,4 +13,9 @@ class ProductController extends Controller
 
         return view('product',['products'=>$data]);
     }
+    function detail($id)
+    {
+        $data= Product::find($id);
+        return view('detail',['product'=>$data]);
+    }
 }
