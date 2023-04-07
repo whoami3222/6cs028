f7bfa9486972e05ee842ba6f947dd22b03f3aa89
David Scesnulevic <2016195@csl-student.wlv.ac.uk>
2023-04-07 13:00:26 +0100

    place order done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -68,6 +68,25 @@ class ProductController extends Controller
  
          return view('ordernow',['total'=>$total]);
     }
+   function orderPlace(Request $req)
+    {
+        $userId=Session::get('user')['id'];
+         $allCart= Cart::where('user_id',$userId)->get();
+         foreach($allCart as $cart)
+         {
+             $order= new Order;
+             $order->product_id=$cart['product_id'];
+             $order->user_id=$cart['user_id'];
+             $order->status="pending";
+             $order->payment_method=$req->payment;
+             $order->payment_status="pending";
+             $order->adress=$req->adress;
+             $order->save();
+             Cart::where('user_id',$userId)->delete(); 
+         }
+         $req->input();
+         return redirect('/');
+    }
 
     
 }
